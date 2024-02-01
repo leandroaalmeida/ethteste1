a52be587b2850c163dab16bce69e5a5e98636bef
leandroaalmeida <leandroaalmeida@users.noreply.github.com>
2024-02-01 19:18:01 -0300

    Update nuclea-aula05.sol

--- nuclea-aula05.sol
+++ nuclea-aula05.sol
@@ -3,6 +3,7 @@ SPDX-License-Identifier: CC-BY-4.0
 (c) Desenvolvido por Jeff Prestes
 This work is licensed under a Creative Commons Attribution 4.0 International License.
 */
+// Endereço do contrato na rede Sepolia 0x2A3072DDdaE4EdcD72A680bBC1D12114D0f269c1
 pragma solidity 0.8.19;
 
 contract Aluguel {
