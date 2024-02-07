96192f75fc0e84a6f2e73b6c97442d1daa5cbc47
leandroaalmeida <leandroaalmeida@users.noreply.github.com>
2024-02-06 21:26:06 -0300

    versao inicial

---
+++ nuclea-aula0603.sol
@@ -0,0 +1,25 @@
+/*
+SPDX-License-Identifier: MIT
+(c) Desenvolvido por Leandro Almeida
+*/
+pragma solidity 0.8.19;
+
+/// @author Leandro Almeida
+/// @title Um exemplo de Amizades
+contract Amizades {
+
+    mapping(address=>mapping(address => uint8)) public amizades;
+
+    event LogNovaAmizade(address eu, address amigo);
+ //   event LogQtdAmizades(address eu, uint8 qtdAmizades);
+
+    // @notice Adicionar amigo
+    // @dev coloca o amigo no mapa do usuario que vai transacionar
+    function adicionar(address _contaAmigo) public {
+        require(msg.sender != _contaAmigo, "Sinto muito. Eu ja sei que vc e seu amigo, preciso de outro");
+        amizades[msg.sender][_contaAmigo] = 1;
+        emit LogNovaAmizade(msg.sender, _contaAmigo);
+//        emit LogQtdAmizades(msg.sender, amizades[msg.sender].length);
+    }
+
+}
