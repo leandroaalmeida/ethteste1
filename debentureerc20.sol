b434d014e276c76eaf5134243c990c25b63fd49f
leandroaalmeida <leandroaalmeida@users.noreply.github.com>
2024-02-08 21:04:20 -0300

    novo

---
+++ debentureerc20.sol
@@ -0,0 +1,269 @@
+pragma solidity 0.8.19;
+
+/**
+ * @title Debenture
+ * @dev Operacoes de uma debenture
+ * @author Jeff Prestes
+ */
+// endereco do contrato 0xF032F1Ac36AA33428e088EBD422D5834307E4Ca0
+/*
+SPDX-License-Identifier: GPL-3.0
+(c) Desenvolvido por Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
+
+pragma solidity 0.8.19;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/// @title Manages the contract owner
+contract Ownable {
+    address payable contractOwner;
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "only owner can perform this operation");
+        _;
+    }
+
+    constructor() { 
+        contractOwner = payable(msg.sender); 
+    }
+    
+    function whoIsTheOwner() public view returns(address) {
+        return contractOwner;
+    }
+
+    function changeOwner(address _newOwner) onlyOwner public returns (bool) {
+        require(_newOwner != address(0x0), "only valid address");
+        contractOwner = payable(_newOwner);
+        return true;
+    }
+    
+}
+interface TokenComPrazo {
+
+    /**
+     * @dev Emitido quando um novo prazo de pagamento é definido
+     */
+    event NovoPrazoPagamento(uint256 prazoAntigo, uint256 prazoNovo);
+
+}
+
+contract Debenture is IERC20, Ownable, TokenComPrazo {
+
+    string _emissor;
+    uint256 immutable _dataEmissao;
+    uint256 public decimals;
+    uint256 _prazoPagamento;
+    string public rating;
+    uint256 _valor;
+
+    mapping (address=>uint256) balances;    
+    mapping (address=>mapping (address=>uint256)) ownerAllowances;
+    modifier hasEnoughBalance(address owner, uint amount) {
+        uint balance;
+        balance = balances[owner];
+        require (balance >= amount); 
+        _;
+    }
+    modifier isAllowed(address spender, address tokenOwner, uint amount) {
+        require (amount <= ownerAllowances[tokenOwner][spender]);
+        _;
+    }
+    modifier tokenAmountValid(uint256 amount) {
+        require(amount > 0);
+        require(amount <= _valor);
+        _;
+    }
+    constructor(string memory emissor_) {
+        _emissor = emissor_;
+        _dataEmissao = block.timestamp;
+        _valor = 0;
+        decimals = 2;
+        _prazoPagamento = _dataEmissao + 90 days;
+        rating = "BBB-";
+        mint(msg.sender, 100000000);
+        emit NovoPrazoPagamento(_dataEmissao, _prazoPagamento);
+    }
+
+    function name() public view returns(string memory) {
+        return string.concat("Token do ", _emissor);
+    }
+
+    function symbol() public view returns(string memory) {
+        return string.concat("DEB-", _emissor);
+    }
+
+    function totalSupply() public override view returns(uint256) {
+        return _valor;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns(uint256) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns(uint256) {
+        return ownerAllowances[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint256 amount) public override  hasEnoughBalance(msg.sender, amount) tokenAmountValid(amount) returns(bool) {
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] = balances[to] + amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    } 
+
+    function transferFrom(address from, address to, uint256 amount) public override 
+    hasEnoughBalance(from, amount) isAllowed(msg.sender, from, amount) tokenAmountValid(amount)
+    returns(bool) {
+        balances[from] = balances[from] - amount;
+        balances[to] += amount;
+        ownerAllowances[from][msg.sender] = ownerAllowances[from][msg.sender] - amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+    
+    function approve(address spender, uint limit) public override returns(bool) {
+        ownerAllowances[msg.sender][spender] = limit;
+        emit Approval(msg.sender, spender, limit);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _valor = _valor + amount;
+        balances[account] = balances[account] + amount;
+        emit Transfer(address(0), account, amount);
+        return true;
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: burn from address");
+
+        require((_valor - amount)>=100, "numero de fracoes baixo");
+
+        balances[account] = balances[account] - amount;
+        _valor = _valor - amount;
+        emit Transfer(account, address(0), amount);
+        return true;
+    }
+
+    /**
+     * @dev Retorna o valor nominal.
+     */
+    function valorNominal() external view returns (uint256) {
+        return _valor;
+    }
+
+    /**
+     * @dev Retorna o nome do Emissor.
+     */
+    function nomeEmissor() external view returns (string memory) {
+        return _emissor;
+    }
+
+    /**
+     * @dev Retorna a data da emissao.
+     */
+    function dataEmissao() external view returns (uint256) {
+        return _dataEmissao;
+    }
+
+    /**
+    * @dev muda o rating
+    * @notice dependendo da situacao economica a empresa avaliadora pode mudar o rating
+    * @param novoRating novo rating da debenture
+    */
+    function mudaRating(string memory novoRating) external onlyOwner returns (bool) {
+        rating = novoRating;
+        return true;
+    }
+
+    function alteraFracoes(uint16 fracoes_) external onlyOwner returns (bool) {
+        require(false, "chama o mint e o burn");
+        return true;
+    }
+
+    /**
+    * @dev retorna o valor da variavel fracoes
+    * @notice informa o numero de fracoes da debenture
+    */
+    function fracoes() external view returns (uint16) {
+        require(false, "chama o totalSupply");
+        return 0;
+    }
+
+ }
